6f14948e4875fc4a9b3e6443f7d68a78eb0d247a
hihoman23 <78002940+hihoman23@users.noreply.github.com>
2023-08-03 12:31:31 +0200

    Update unit-ping.lua

--- unit-ping.lua
+++ unit-ping.lua
@@ -1,1 +1,12 @@
+function widget:GetInfo()
+    return {
+        name    = "Attacked Unit Ping",
+        desc    = "Adds button that sends camera to attacked units.",
+        author  = "hihoman23",
+        date    = "2023",
+        license = "GNU GPL, v3 or later",
+        layer   = 0,
+        enabled = true --  loaded by default?
+    }
+end
 
