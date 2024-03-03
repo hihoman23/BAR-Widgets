cc199d7d0ae14a0d60bca22fef973f46d88d4624
hihoman23 <78002940+hihoman23@users.noreply.github.com>
2024-03-03 14:20:23 +0100

    Add files via upload

--- export_data_as_csv.lua
+++ export_data_as_csv.lua
@@ -20,7 +20,7 @@ local ignoreList = {
 
 local data = {}
 local frame = 0
-
+local isSpec
 local teamList = Spring.GetTeamList()
 local teamCount = 0
 
@@ -34,7 +34,7 @@ local playerTable = {}
 local function createPlayerTable()
     for _, team in ipairs(teamList) do
         local playerName
-        local _, leader, _, ai, _, allyTeamID = GetTeamInfo(team)
+        local _, leader, _, ai = GetTeamInfo(team)
         if leader then
             if ai then
                 _, playerName = GetAIInfo(team)
@@ -82,6 +82,65 @@ local function tableToCSV(tbl, name, finalFrame)
     end
 end
 
+local function addStats(hist)
+    hist.damageEfficiency = 0
+    if not (hist.damageReceived == 0) then
+        hist.damageEfficiency = (hist.damageDealt/hist.damageReceived)*100
+    end
+
+    return hist
+end
+
+local function createTable()
+    teamCount = 0
+    local dataTable = {}
+    local finalFrame
+    local timeInterval = math.ceil(timeInterval/450)
+    for _,teamID in ipairs(teamList) do
+        if teamID ~= GetGaiaTeamID() then
+            local range = GetTeamStatsHistory(teamID)
+            local history = GetTeamStatsHistory(teamID,0,range)
+            if history then
+                teamCount = teamCount + 1
+                for i = 1, range, timeInterval do
+                    for stat, val in pairs(addStats(history[i])) do
+                        if not ignoreList[stat] then
+                            local statTable = dataTable[stat]
+                            if statTable then
+                                local playerStat = dataTable[stat][teamID]
+                                if playerStat then
+                                    playerStat[#playerStat+1] = val
+                                else
+                                    statTable[teamID] = {val}
+                                end
+                            else
+                                dataTable[stat] = {[teamID] = {val}}
+                            end
+                        end
+                    end
+                end
+                if not ((range%timeInterval)==0) then
+                    for stat, val in pairs(history[#history]) do
+                        if stat == "frame" then
+                            finalFrame = val
+                        end
+                        if not ignoreList[stat] then
+                            local playerStat = dataTable[stat][teamID]
+                            if playerStat then
+                                playerStat[#playerStat+1] = val
+                            end
+                        end
+                    end
+                end
+                if not finalFrame then
+                    finalFrame = range
+                end
+            end
+        end
+    end
+    data = dataTable
+end
+
 local function addCurrentData(force)
     if ((frame%timeInterval)==0) or force then
         for _,teamID in ipairs(teamList) do
@@ -92,12 +151,7 @@ local function addCurrentData(force)
                     teamCount = teamCount + 1
                     history = history[#history]
 
-                    history.damageEfficiency = 0
-                    if not (history.damageReceived == 0) then
-                        history.damageEfficiency = (history.damageDealt/history.damageReceived)*100
-                    end
-
-                    for stat, val in pairs(history) do
+                    for stat, val in pairs(addStats(history)) do
                         if not ignoreList[stat] then
                             local statTable = data[stat]
                             if statTable then
@@ -131,7 +185,12 @@ local function createName()
 end
 
 local function saveData()
-    addCurrentData(true)
+    if isSpec then
+        addCurrentData(true)
+    else
+        createTable()
+        Spring.Echo("Save will be more low resolution")
+    end
     tableToCSV(data, createName(), frame)
     Spring.Echo("Resource Data Saved")
 end
@@ -140,9 +199,7 @@ function widget:Initialize()
     timeInterval = timeInterval*30
     widgetHandler:AddAction("save_resource_data", saveData, nil, "p")
 
-    if not Spring.GetSpectatingState() then
-        widgetHandler:RemoveWidget(self)
-    end
+    isSpec = Spring.GetSpectatingState()
 end
 
 function widget:GameStart()
